e111bf801d7bd5ee68a39d3829edbbc63f043ca5
ljollans <lee.jollans@gmail.com>
2017-02-14 15:45:44 +0000

    Add files via upload

---
+++ RAFT_collect_2nd_level_memorysave.m
@@ -0,0 +1,35 @@
+function RAFT_collect_2nd_level_memorysave(design)
+% collects and combines intermittent files saved in RAFT_2nd_level
+%
+% for comments and questions please contact lee.jollans@gmail.com
+
+cd(design.saveto);
+design.nvars=size(design.data,2);
+fprintf('Aggregating Middle Fold Analysis and deleting intermediate files\n', size(design.data));
+%%
+for mainfold=1:design.numFolds
+    eval(['LHmerit' num2str(mainfold) '=NaN(design.numFolds, design.numFolds, size(design.merit_thresholds,2),design.numAlphas, design.numLambdas);']);
+    eval(['vars2use' num2str(mainfold) '=zeros(design.numFolds, design.numFolds, size(design.merit_thresholds,2),design.numAlphas, design.numLambdas,size(design.data,2));']);
+    eval(['lambda_values' num2str(mainfold) '=NaN(design.numFolds, design.numFolds, size(design.merit_thresholds,2),design.numAlphas, design.numLambdas);']);
+    for subfolds=1:design.numFolds
+        load([design.saveto filesep 'Second_level_results_' num2str(mainfold) '_' num2str(subfolds)]);
+        eval(['LHmerit' num2str(mainfold) '(subfolds,:,:,:,:)=LHmerit2save;']);
+        eval(['vars2use' num2str(mainfold) '(subfolds,:,:,:,:,:)=vars2use2save;']);
+        eval(['lambda_values' num2str(mainfold) '(subfolds,:,:,:,:)=lambda_values2save;']);
+    end
+    eval(['vars2use' num2str(mainfold) '=logical(vars2use' num2str(mainfold) ');']);
+    fname=['LHmerit' num2str(mainfold)];  save(fname, fname);
+    fname=['lambda_values' num2str(mainfold)];  save(fname, fname);
+    fname=['vars2use' num2str(mainfold)];
+    try
+      save(fname, fname);
+    catch
+      save(fname, fname, '-v7.3');
+    end
+end
+
+for folds=1:(design.numFolds*design.numFolds)
+        [mainfold subfolds]=ind2sub([design.numFolds design.numFolds], folds);  
+        delete([design.saveto filesep 'Second_level_results_' num2str(mainfold) '_' num2str(subfolds) '.mat']);
+end
+end
